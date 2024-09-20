6430ec2ec8e0ade88b3b4a46bc8ce099b40544e2
Dries Willekens <Dries.Willekens.Business@hotmail.com>
2024-09-20 20:59:46 +0200

    BMP280 test completed

--- Comptest/BMP_280.py
+++ Comptest/BMP_280.py
@@ -1,1 +1,21 @@
 #BMP_280 i2c test code
+#Select librarys
+import os
+from time import sleep
+import smbus
+from bmp280 import BMP280
+
+#configure sensor
+i2c = smbus.SMBus(1)
+bmp280 =  BMP280(i2c_dev=i2c)
+
+try:
+    while True:
+        os.system('clear')
+        temperature = bmp280.get_temperature()
+        pressure = bmp280.get_pressure()
+        print(f"{temperature:05.2f}*C")
+        sleep(1)
+
+except KeyboardInterrupt:
+    os.system('clear')
